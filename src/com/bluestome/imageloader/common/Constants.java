0f0eed46cd55b02defcc123db8b688b4454a6a02
bluestome <zhangxiao918@21cn.com>
2013-08-14 00:00:42 +0800

    2013-08-14 新增数据库名属性。

--- src/com/bluestome/imageloader/common/Constants.java
+++ src/com/bluestome/imageloader/common/Constants.java
@@ -7,5 +7,7 @@ public class Constants {
     public static final String URL = "http://www.6188.com";
     public static final String IMAGE_PREFIX_URL = "http://pic.6188.com/upload_6188s/";
 
+    public static final String DB_NAME = "imageloader.db";
+
     public static final String DESTORY_SELF_ACTION = "com.bluestome.imageloader.DESTORY_SELF_ACTION";
 }
